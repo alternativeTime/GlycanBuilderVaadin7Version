42732858965db4ee01e2ecc6b2ba69ebb4f71bd4
alternativeTime <matthew.campbell1980@gmail.com>
2014-09-16 11:02:57 +1000

    minor change including headers

--- glycanbuilderv/src/ac/uk/icl/dell/vaadin/canvas/basiccanvas/BasicCanvas.java
+++ glycanbuilderv/src/ac/uk/icl/dell/vaadin/canvas/basiccanvas/BasicCanvas.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2014. Matthew Campbell <matthew.campbell@mq.edu.au>, David R. Damerell <david@nixbioinf.org>.
+ *
+ * This file is part of GlycanBuilder Vaadin Release and its affliated projects EUROCarbDB, UniCarb-DB and UniCarbKB.
+ *
+ * This program is free software free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GlycanBuilder Vaadin Release is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License (LICENSE.txt) for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GlycanBuilder Vaadin Release.  If not, see <http ://www.gnu.org/licenses/>.
+ */
+
 package ac.uk.icl.dell.vaadin.canvas.basiccanvas;
 
 import java.util.ArrayList;
--- glycanbuilderv/src/ac/uk/icl/dell/vaadin/canvas/shapes/Square.java
+++ glycanbuilderv/src/ac/uk/icl/dell/vaadin/canvas/shapes/Square.java
@@ -1,22 +1,21 @@
 /*
-*   EuroCarbDB, a framework for carbohydrate bioinformatics
-*
-*   Copyright (c) 2006-2011, Eurocarb project, or third-party contributors as
-*   indicated by the @author tags or express copyright attribution
-*   statements applied by the authors.  
-*
-*   This copyrighted material is made available to anyone wishing to use, modify,
-*   copy, or redistribute it subject to the terms and conditions of the GNU
-*   Lesser General Public License, as published by the Free Software Foundation.
-*   A copy of this license accompanies this distribution in the file LICENSE.txt.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-*   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
-*   for more details.
-*   
-*   @author David R. Damerell (david@nixbioinf.org)
-*/
+ * Copyright (c) 2014. Matthew Campbell <matthew.campbell@mq.edu.au>, David R. Damerell <david@nixbioinf.org>.
+ *
+ * This file is part of GlycanBuilder Vaadin Release and its affliated projects EUROCarbDB, UniCarb-DB and UniCarbKB.
+ *
+ * This program is free software free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GlycanBuilder Vaadin Release is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License (LICENSE.txt) for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GlycanBuilder Vaadin Release.  If not, see <http ://www.gnu.org/licenses/>.
+ */
 package ac.uk.icl.dell.vaadin.canvas.shapes;
 
 import org.eurocarbdb.application.glycanbuilder.ResidueStyle;
