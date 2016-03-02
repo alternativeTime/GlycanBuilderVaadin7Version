eb4cdd98fa8303557258ce43a5debd9a48f59c1c
Davide Alocci <alodavide@gmail.com>
2016-03-02 15:38:59 +0100

    Enabling glycan builder for CORS

--- glycanbuilderv/src/ac/uk/icl/dell/vaadin/glycanbuilder/GlycanBuilderWindow.java
+++ glycanbuilderv/src/ac/uk/icl/dell/vaadin/glycanbuilder/GlycanBuilderWindow.java
@@ -18,10 +18,14 @@
  */
 package ac.uk.icl.dell.vaadin.glycanbuilder;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.eurocarbdb.application.glycanbuilder.BuilderWorkspace;
 import org.eurocarbdb.application.glycanbuilder.GlycanParserFactory;
@@ -50,6 +54,62 @@ public class GlycanBuilderWindow extends UI{
 	@VaadinServletConfiguration(productionMode = true, ui = GlycanBuilderWindow.class, widgetset = "ac.uk.icl.dell.vaadin.glycanbuilder.widgetset.GlycanbuilderWidgetset")
 	public static class Servlet extends VaadinServlet {
 		private static final long serialVersionUID = 4956233270191237091L;
+        /**
+         * Override to handle the CORS requests.
+         */
+        @Override
+        protected void service(HttpServletRequest request,
+                HttpServletResponse response) throws ServletException,
+                IOException {
+
+            // Origin is needed for all CORS requests
+            String origin = request.getHeader("Origin");
+            if (origin != null && isAllowedRequestOrigin(origin)) {
+
+                // Handle a preflight "option" requests
+                if ("options".equalsIgnoreCase(request.getMethod())) {
+                    response.addHeader("Access-Control-Allow-Origin", origin);
+                    response.setHeader("Allow",
+                            "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS");
+ 
+                    // allow the requested method
+                    String method = request
+                            .getHeader("Access-Control-Request-Method");
+                    response.addHeader("Access-Control-Allow-Methods", method);
+
+                    // allow the requested headers
+                    String headers = request
+                            .getHeader("Access-Control-Request-Headers");
+                    response.addHeader("Access-Control-Allow-Headers", headers);
+
+                    response.addHeader("Access-Control-Allow-Credentials",
+                            "true");
+                    response.setContentType("text/plain");
+                    response.setCharacterEncoding("utf-8");
+                    response.getWriter().flush();
+                    return;
+                } // Handle UIDL post requests
+                else if ("post".equalsIgnoreCase(request.getMethod())) {
+                    response.addHeader("Access-Control-Allow-Origin", origin);
+                    response.addHeader("Access-Control-Allow-Credentials",
+                            "true");
+                    super.service(request, response);
+                    return;
+                }
+            }
+
+            // All the other requests nothing to do with CORS
+            super.service(request, response);
+
+        }
+
+        /**
+         * Check that the page Origin header is allowed.
+         */
+        private boolean isAllowedRequestOrigin(String origin) {
+            // TODO: Remember to limit the origins.
+            return origin.startsWith("*.expasy.org") || origin.startsWith("*.isb-sib.ch");
+        }
 	}
 	private GlycanBuilder theBuilder; 
 	private static final long serialVersionUID=-4407090778568443024L;
